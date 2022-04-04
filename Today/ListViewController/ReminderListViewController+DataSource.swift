37fa52c23e2fcdd6ec91040c528b3ad4b0d4e885
FrancaTM <francatm@outlook.com>
2022-04-04 18:27:48 -0300

    reorganize view controller add data source extension for reminderListVC

---
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,22 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  Today
+//
+//  Created by Tulio Marcos Franca on 04/04/22.
+//
+
+import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+    }
+}

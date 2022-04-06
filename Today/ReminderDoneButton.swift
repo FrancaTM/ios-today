f96cbac542d5e7738798ecb55a995bff8089c017
FrancaTM <francatm@outlook.com>
2022-04-06 14:22:58 -0300

    create custom button action

---
+++ Today/ReminderDoneButton.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Tulio Marcos Franca on 06/04/22.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+    
+}

7d33b8600c0626931da3fab1d3786a24a8b3e6cf
FrancaTM <francatm@outlook.com>
2022-04-11 20:36:50 -0300

    create sections for editing mode

---
+++ Today/DetailViewController/ReminderViewController+Section.swift
@@ -0,0 +1,30 @@
+//
+//  ReminderViewController+Section.swift
+//  Today
+//
+//  Created by Tulio Marcos Franca on 11/04/22.
+//
+
+import Foundation
+
+extension ReminderViewController {
+    enum Section: Int, Hashable {
+        case view
+        case title
+        case date
+        case notes
+        
+        var name: String {
+            switch self {
+            case .view:
+                return ""
+            case .title:
+                return NSLocalizedString("Title", comment: "Title section name")
+            case .date:
+                return NSLocalizedString("Date", comment: "Date section name")
+            case .notes:
+                return NSLocalizedString("Notes", comment: "Notes section name")
+            }
+        }
+    }
+}

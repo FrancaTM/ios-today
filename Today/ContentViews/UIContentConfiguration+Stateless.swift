a99fb912df2d65933b2837ea841cfdb1b55e75fc
FrancaTM <francatm@outlook.com>
2022-04-19 14:28:51 -0300

    conform to content view protocol

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Tulio Marcos Franca on 19/04/22.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
